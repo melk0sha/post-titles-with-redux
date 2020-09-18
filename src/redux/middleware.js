5b67c84ff56d9436aef14f6f4c96d49bc0bc9660
melk0sha <din.ivanova13@gmail.com>
2020-09-18 13:45:25 +0300

    feat: added redux reducers, types and actions

---
+++ src/redux/middleware.js
@@ -0,0 +1,22 @@
+import { showAlert } from "./actions";
+import { CREATE_POST } from "./types";
+
+const forbiddenWords = ["fuck", "spam", "php"];
+
+export function forbiddenWordsMiddleware({ dispatch }) {
+  return function (next) {
+    return function (action) {
+      if (action.type === CREATE_POST) {
+        const found = forbiddenWords.filter((word) =>
+          action.payload.title.includes(word)
+        );
+
+        if (found.length) {
+          return dispatch(showAlert("You are a spamer"));
+        }
+      }
+
+      return next(action);
+    };
+  };
+}
